b9515010d155d5f5b97cce98c235b4a06f0fdc29
Alex-Hamblin <alexmachamblin@gmail.com>
2024-01-27 12:47:18 -0500

    added the ability to change brush size and fixed some of the colours

--- Assets/Scripts/Colour.cs
+++ Assets/Scripts/Colour.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Colour : MonoBehaviour
 {
     public LineRenderer linerenderer;
     public GameObject[] colours;
+    public int width = 1;
 
 
     private void Start()
     {
         linerenderer = GetComponent<LineRenderer>();
         gameObject.GetComponent<draw1>().brush = colours[0];
+        
         
 
     }
@@ -45,6 +48,18 @@ public class Colour : MonoBehaviour
             gameObject.GetComponent<draw1>().brush = colours[5];
 
         }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            gameObject.GetComponent<draw1>().brush.GetComponent<LineRenderer>().widthMultiplier = 0.5f;
+        }
 
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            gameObject.GetComponent<draw1>().brush.GetComponent<LineRenderer>().widthMultiplier = 1.0f;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            gameObject.GetComponent<draw1>().brush.GetComponent<LineRenderer>().widthMultiplier = 2.0f;
+        }
     }
 }
